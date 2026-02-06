ac100f79acc03aaa9f9e51aa4285f3cb8a1bb90f
Bowlofneighs <216767681+bowlofneighs@users.noreply.github.com>
2026-02-06 15:11:32 -0500

    set up the main thingie

---
+++ main.py
@@ -0,0 +1,16 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
+# send a message
+client.chat_postMessage(channel='#neighsofbowl-testing', text="PLEASE FREE MEEEEE @bowlofneighs")
+
+
+
